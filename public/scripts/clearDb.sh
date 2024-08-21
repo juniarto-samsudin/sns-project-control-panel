f6eacdf97b6a0cf54c09dc35b6b82a7e485217e3
Juniarto Samsudin <juniarto_samsudin@ihpc.a-star.edu.sg>
2024-08-21 01:46:08 +0000

    change influxdb token

--- public/scripts/clearDb.sh
+++ public/scripts/clearDb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-token='V6Mcr-5AOJFtWPNnT-9us4gu799kcyX-W2xnWdOQTd-LC36M5gOJMoiD5skM6pzD2HO3XavEKWdkJM9PMOeyYw=='
+token='Z8PIzBNZcT7cpgvJusDBVTaWXd1tzo9RJin2O2OQpaOOBzOEO9TAClGjPXR_IpmMHtqr4fAeUeOQklKyLdtsNw=='
 host='http://10.9.240.14:8086'
 
 ./public/scripts/influx delete --host $host --token $token  --bucket sample-bucket --predicate '_measurement="Bitcoin-Prediction"' --start 1970-01-01T00:00:00Z --stop $current_date --org IHPC
